8fe2ac339cfcac3e91885a49762be4efa1b509a9
bag3318 <bglatmman@outlook.com>
2017-10-10 18:46:13 -0700

    fixed constatns methods

--- lib/Constants.rb
+++ lib/Constants.rb
@@ -5,18 +5,18 @@ module Constants
   CHOICES = [['r', 'rock'], ['p', 'paper'], ['s', 'scissors']]
   NTRY_TO_SYM = { # define entry to symbol (key to value)
     CHOICES[0][0] => :ROCK    , CHOICES[0][1]     => :ROCK    ,
-    CHOICES[1][0] => :PAPER   , CHOICES[1][1]   => :PAPER   ,
-    's' => :SCISSORS, 'scissors' => :SCISSORS
+    CHOICES[1][0] => :PAPER   , CHOICES[1][1]     => :PAPER   ,
+    CHOICES[2][0] => :SCISSORS, CHOICES[2][1]     => :SCISSORS
   } 
   VALID_ENTRIES = NTRY_TO_SYM.keys # create valid entries
   COMPUTER_CHOICES = NTRY_TO_SYM.values # define computer choices
   WINNERS = [
     # format: player choice, computer choice
-    [:SCISSORS, :PAPER], 
-    [:PAPER   , :ROCK], 
+    [:SCISSORS, :PAPER   ], 
+    [:PAPER   , :ROCK    ], 
     [:ROCK    , :SCISSORS]
   ] 
-  LOSERS = WINNERS.map { |pc,cc| [cc,pc] } # this will take the original WINNERS array and flip the symbols, thus returning a loss for the user/player
+  LOSERS = WINNERS.map { |winning_choice,losing_choice| [losing_choice,winning_choice] } # this will take the original WINNERS array and flip the symbols, thus returning a loss for the user/player
   INIT_STRINGS = [
     ColorizedString["You are about to enter a rock-paper-scissors best of 3 match."].colorize(:green), 
     ColorizedString["Press the return/enter key to continue..."].colorize(:green), 
