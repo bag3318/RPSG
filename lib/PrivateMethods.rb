984617b3889370064b12c38d2f8ea649ab76d279
bag3318 <bglatmman@outlook.com>
2017-10-26 11:40:10 -0700

    fix

--- lib/PrivateMethods.rb
+++ lib/PrivateMethods.rb
@@ -26,7 +26,7 @@ module PrivateMethods
     def final_outcome(pl,co)
       return :WIN  if pl > co
       return :LOSE if pl < co
-      return :TIE  if pl = co
+      # return :TIE  if pl = co
       # there will never be a tie for the final outcome due to the code in the play() method
     end
   end
